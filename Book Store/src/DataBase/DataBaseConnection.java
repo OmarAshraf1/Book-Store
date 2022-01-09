d6319d0fcbbe90d0266e6016912d84123d268b81
Omar Ashraf <53156581+OmarAshraf1@users.noreply.github.com>
2022-01-09 08:21:17 -0800

    Add files via upload

---
+++ Book Store/src/DataBase/DataBaseConnection.java
@@ -0,0 +1,10 @@
+package DataBase;
+
+public class DataBaseConnection {
+	private String url = "jdbc:mysql://127.0.0.1:3300/csedconnections";
+    private String username = "root";
+    private String password = "";
+    private String tableName;
+    
+    
+}
